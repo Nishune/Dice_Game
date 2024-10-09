a32a526384939f0ecb035a611bd932922d1aaea5
Rikard Engström <rikardengstrom89@gmail.com>
2024-10-09 13:45:10 +0200

    Fixed som JS for readability and added variables for saving player score

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
+const myBtn = document.querySelector("#myBtn");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+
+
+myBtn.addEventListener("click", rollDice);
+
+
+
+
+function rollDice() {
+
 let dice1 = Math.floor((Math.random() * 6) + 1);
 let dice2 = Math.floor((Math.random() * 6) + 1);
+player1Score = dice1;
+player2Score = dice2;
 
 if (dice1 === 1) {
-    document.querySelector("#player1").setAttribute("src", "./images/dice1.png");
+    player1.setAttribute("src", "./images/dice1.png");
 }
 if (dice2 === 1) {
-    document.querySelector("#player2").setAttribute("src", "./images/dice1.png");
+    player2.setAttribute("src", "./images/dice1.png");
 }
 if (dice1 === 2) {
-    document.querySelector("#player1").setAttribute("src", "./images/dice2.png");
+    player1.setAttribute("src", "./images/dice2.png");
 }
 if (dice2 === 2) {
-    document.querySelector("#player2").setAttribute("src", "./images/dice2.png");
+    player2.setAttribute("src", "./images/dice2.png");
 }
 if (dice1 === 3) {
-    document.querySelector("#player1").setAttribute("src", "./images/dice3.png");
+    player1.setAttribute("src", "./images/dice3.png");
 }
 if (dice2 === 3) {
-    document.querySelector("#player2").setAttribute("src", "./images/dice3.png");
+    player2.setAttribute("src", "./images/dice3.png");
 }
 if (dice1 === 4) {
-    document.querySelector("#player1").setAttribute("src", "./images/dice4.png");
+    player1.setAttribute("src", "./images/dice4.png");
 }
 if (dice2 === 4) {
-    document.querySelector("#player2").setAttribute("src", "./images/dice4.png");
+    player2.setAttribute("src", "./images/dice4.png");
 }
 if (dice1 === 5) {
-    document.querySelector("#player1").setAttribute("src", "./images/dice5.png");
+    player1.setAttribute("src", "./images/dice5.png");
 }
 if (dice2 === 5) {
-    document.querySelector("#player2").setAttribute("src", "./images/dice5.png");
+    player2.setAttribute("src", "./images/dice5.png");
 }
 if (dice1 === 6) {
-    document.querySelector("#player1").setAttribute("src", "./images/dice6.png");
+    player1.setAttribute("src", "./images/dice6.png");
 }
 if (dice2 === 6) {
-    document.querySelector("#player2").setAttribute("src", "./images/dice6.png");
+    player2.setAttribute("src", "./images/dice6.png");
+}
+if (player1Score > player2Score) {
+    
+}
 }
